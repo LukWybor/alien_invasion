2600469fe141d86f3e391259f7eff920fe22e3ac
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-04 17:41:49 +0100

    Update alien_invasion.py
    Define the background color
    Refresh the screen during each iteration of the main loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,16 @@
 import sys
+
 import pygame
 
 def run_game():
+	
     #Initialize the game and create a screen object
     pygame.init()
     screen = pygame.display.set_mode((1200,600))
     pygame.display.set_caption("Alien invasion")
+    
+    #Defining the background color
+    bg_color = (100,160,230)
     
     #Starting the main game loop
     while True:
@@ -14,6 +19,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        #Refresh the screen during each iteration of the loop
+        screen.fill(bg_color)
+        
         
         #Display the last modified screen
         pygame.display.flip()
