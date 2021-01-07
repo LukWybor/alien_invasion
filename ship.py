53126b67056bb60f5d40029c2ff9cd8bdb6f083b
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-07 14:57:54 +0100

    Update ship.py

--- ship.py
+++ ship.py
@@ -14,7 +14,7 @@ class Ship():
         
         #Initialize new ship at the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottom-20
         
     def blitme(self):
         """Draw the ship at its current location"""
