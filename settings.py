aaeaf8d56d817c1c23888a9c54444e9943ebd373
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-07 14:18:46 +0100

    Create settings.py
    Creating a "setting" class.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Store all settings"""
+    
+    def __init__(self):
+        """Initialize the game's settings"""
+        #screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colot = (100,160,230)
