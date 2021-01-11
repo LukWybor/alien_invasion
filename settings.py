296702acd546869bd0f6a6a2cf3a2bfc00c259fa
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-11 20:37:46 +0100

    Update settings.py
    Added sheep speed factor.

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ class Settings():
         """Initialize the game's settings"""
         #screen
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (100,160,180)
+        self.ship_speed_factor = 1.5
