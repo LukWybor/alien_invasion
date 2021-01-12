02536708a9dd5ef1770c770b35311707ba23562c
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-12 14:03:12 +0100

    Update settings.py
    Added bullet settings.

--- settings.py
+++ settings.py
@@ -3,8 +3,13 @@ class Settings():
     
     def __init__(self):
         """Initialize the game's settings"""
-        #screen
+        #screen settings
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (100,160,180)
         self.ship_speed_factor = 1.5
+        #bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
