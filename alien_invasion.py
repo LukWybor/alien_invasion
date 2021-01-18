840f3c2bd3acf4fb7397211f162a5bf2612d4d35
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-18 20:33:59 +0100

    Update alien_invasion.py
    Refactoring the code.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,13 +23,7 @@ def run_game():
     while True: 
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        
-        #Delete bullets that have disappeared
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        
+        gf.update_bullets(bullets)        
         gf.update_screen(game_settings, screen, ship, bullets)
         
         
