e7c63461205287a58be2c1f3dc32a813cb9479a0
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-25 16:58:43 +0100

    Create alien.py
    Alien class which represent a single alien in the fleet.

---
+++ alien.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Represent a single alien in the fleet."""
+    def __init__(self, game_settings, screen):
+        """Initialize the alien and set its starting position"""
+        super().__init__()
+        self.screen = screen
+        self.game_settings = game_settings
+    
+        #Load the alien image and difine its rect attribute
+        self.image = pygame.image.load('images/alien.png')
+        self.image = pygame.transform.scale(self.image, (30, 60))
+        self.rect = self.image.get_rect()
+        
+        #Initialize each new alien near the top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+        #Store the exact position of the alien
+        self.x = float(self.rect.x)
+        
+    def blitme(self):
+        """Display the alien at his current position"""
+        self.screen.blit(self.image, self.rect)
