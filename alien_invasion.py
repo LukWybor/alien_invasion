028842a37e2d0968dd01428e1a4c3b807d2a757a
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-25 17:05:28 +0100

    Update alien_invasion.py
    Added an instance of the alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -16,6 +17,9 @@ def run_game():
     #Make a ship
     ship = Ship(game_settings, screen)
     
+    #Make an alien
+    alien = Alien(game_settings, screen)
+    
     #Bullets are stored in group
     bullets = Group()
     
@@ -24,7 +28,6 @@ def run_game():
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)        
-        gf.update_screen(game_settings, screen, ship, bullets)
-        
+        gf.update_screen(game_settings, screen, ship, alien, bullets)
         
 run_game()
