22312d1cad240d880539246c6f948378c81ec446
LukWybor <57914554+LukWybor@users.noreply.github.com>
2021-01-25 17:36:09 +0100

    Update settings.py
    Changed bullets color

--- settings.py
+++ settings.py
@@ -12,5 +12,5 @@ class Settings():
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (200,60,60)
+        self.bullet_color = (0,255,255)
         self.bullets_allowed = 3
